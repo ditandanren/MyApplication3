e00f22d85d30a0752c6e7fe4e12f20101dc5dfef
suyonggang <1109832213@qq.com>
2019-12-10 10:39:12 +0800

    branch001

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        Logfsfkasofejgejjkfjkdsfjkdfdfdfs
+//        Logfsfkasofejgejjkfjkdsfjkdfdfdfsbranch001111111111
     }
 }
